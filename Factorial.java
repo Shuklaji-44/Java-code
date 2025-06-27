87ad7905f980a6be21aeed500ac4344449048ad9
Shuklaji-44 <shuklagauravakela@gmail.com>
2025-06-27 08:45:13 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,13 @@
+class Factorial
+{
+public static void main(String[] args)
+{
+ int f=1,n=6,i;
+ for(i=1;i<=n;i++)
+    {
+         f=f*i;
+   }
+    System.out.println("the factorial of 6 is"+f);
+}
+}
+  
